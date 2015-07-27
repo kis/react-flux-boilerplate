c8abd2a5b3676e09dfa0f2a7043648b32f256d8f
Kirill Stepkin <kirillstyopkin@gmail.com>
2015-07-27 17:39:43 +0300

    events

--- views/index.jsx
+++ views/index.jsx
@@ -13,12 +13,40 @@ var TodoBox = React.createClass({
 });
 
 var TodoList = React.createClass({
+  getInitialState: function() { 
+    return { 
+      data: this.props.data, 
+      titleValue: "", 
+      detailValue: "" 
+    }; 
+  }, 
+  changeTitle: function(e) { 
+    this.setState({titleValue: e.target.value});
+  }, 
+  changeDetail: function(e) { 
+    this.setState({detailValue: e.target.value});
+  }, 
+  addTodo: function() { 
+    this.props.data.push({
+      title: this.state.titleValue,
+      detail: this.state.detailValue
+    });
+    this.setState({
+      titleValue: '',
+      detailValue: ''
+    });
+  },
   render: function() {
     var todo = this.props.data.map(function(obj) {
       return <Todo title={obj.title}>{obj.detail}</Todo>;
     });
     return (
       <div className = "todoList">
+        <div> 
+          Title:<input type="text" value={this.state.titleValue} onChange={this.changeTitle} />
+          Detail:<input type="text" value={this.state.detailValue} onChange={this.changeDetail} /> 
+          <button onClick={this.addTodo}>Add</button> 
+        </div>
         <table style={style.tableContent}>
           <tbody>
             {todo}
